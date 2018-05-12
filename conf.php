5f7e05d6190937a90d281b1e079143d5e317f38c
jatinder-pal <boskim.3ginfio@gmail.com>
2018-05-12 12:29:36 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
        
-	define('SHOPIFY_APP_API_KEY', 'a1d8a4ace6eaa1e0241afa2dcd19132c');
-	define('SHOPIFY_APP_SHARED_SECRET', 'fdf3bce4b18ab3c6bf490118795dda47');
+	define('SHOPIFY_APP_API_KEY', '7d071492b7d917d8d957b232f7fbe77a');
+	define('SHOPIFY_APP_SHARED_SECRET', '4d138944043cad64d5a0ab0645f9e2b8');
 	define('APP_SERVER_URL','https://collection-customize.herokuapp.com/');
 ?>
 
